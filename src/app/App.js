b23e81ee41e46027edec97a8ebf1fa46a370653b
MilanMixa <stankovicmilan990@gmail.com>
2021-08-30 22:16:26 +0200

    initial header style added, installed router-dom

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,17 @@
+import { Fragment } from 'react';
+import {Switch, Route} from "react-router-dom";
+
+import Header from './Header/Header';
+
 import './App.css';
+import { from } from 'pumpify';
 
 function App() {
-
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
 }
 
 export default App;
--- src/app/Header/Navigation.jsx
+++ src/app/Header/Navigation.jsx
@@ -2,10 +2,10 @@ import "./Navigation.css";
 
 const Navigation = () => {
     return (
-        <ul>
-            <li>Home</li>
-            <li>Authors</li>
-            <li>About</li>
+        <ul className="d-flex">
+            <li className="pe-3 border-end">Home</li>
+            <li className="px-3 border-end">Authors</li>
+            <li className="ps-3">About</li>
         </ul>
     )
 };
