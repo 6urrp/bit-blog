fc08ae7e4268e6af4e1a432cc2fc4ee28b0803b3
MilanMixa <stankovicmilan990@gmail.com>
2021-08-30 21:35:04 +0200

    Header and Navigation components added

---
+++ src/app/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+import Navigation from "./Navigation";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>RND BLG</h1>
+            <Navigation />
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/app/Header/Navigation.jsx
@@ -0,0 +1,13 @@
+import "./Navigation.css";
+
+const Navigation = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Authors</li>
+            <li>About</li>
+        </ul>
+    )
+};
+
+export default Navigation;
