4b6d3b746ae6b443651ece87dea0837a176bb413
MilanMixa <stankovicmilan990@gmail.com>
2021-09-02 18:57:32 +0200

    posts from the same author added

--- src/app/Posts/SinglePost.jsx
+++ src/app/Posts/SinglePost.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 import { getSinglePost } from "../../services/getSinglePost";
 import { getAuthor } from "../../services/getAuthor";
+import { getPostsFromAuthor } from "../../services/getPostsFromAuthor";
 
 const SinglePost = props => {
     const [singlePost, setSinglePost] = useState(null);
     const [author, setAuthor] = useState(null);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         getSinglePost(props.match.params.id)
@@ -20,6 +22,13 @@ const SinglePost = props => {
         }
     }, [singlePost])
 
+    useEffect(() =>{
+        if(author) {
+            getPostsFromAuthor(author.id)
+            .then(posts => setPosts(posts))
+        }
+    },[author])
+
     return (
             !author
             ? <h1>Loading...</h1>
@@ -32,8 +41,13 @@ const SinglePost = props => {
                 <h5 className="mb-5"><Link to={`/author/${author.id}`}>{author.name}</Link></h5>
                 <p>{singlePost.body}</p>
             </div>
-            <div>
-
+            <div className="w-75 mx-auto mt-3">
+                <h3>{posts.length} more posts from the same author</h3>
+                <ul className="ps-0">
+                    {
+                        posts.map((post, index) => <li className="fs-5" key={index}> <Link to="/">{index+1}. {post.title.slice(0, 7)} - {post.body.slice(0, 15)}</Link></li>)
+                    }
+                </ul>
             </div>
         </Fragment>
     )
